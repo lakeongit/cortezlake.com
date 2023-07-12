b34bafee6d3868785143ce12756ffe94d57fbf93
Cortez Lake <122133828+lakeongit@users.noreply.github.com>
2023-07-12 03:01:02 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Add your interactivity here. For example, a smooth scroll:
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
